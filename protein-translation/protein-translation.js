76df0639ab04f9a9c9a4f9094c7d827a4d027dfe
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-09-14 20:46:35 +0300

    "Protein Translation" exercise

---
+++ protein-translation/protein-translation.js
@@ -0,0 +1,40 @@
+const PROTEINS = {
+  "AUG": "Methionine",
+  "UUU": "Phenylalanine",
+  "UUC": "Phenylalanine",
+  "UUA": "Leucine",
+  "UUG": "Leucine",
+  "UCU": "Serine",
+  "UCC": "Serine",
+  "UCA": "Serine",
+  "UCG": "Serine",
+  "UAU": "Tyrosine",
+  "UAC": "Tyrosine",
+  "UGU": "Cysteine",
+  "UGC": "Cysteine",
+  "UGG": "Tryptophan",
+  "UAA": "STOP",
+  "UAG": "STOP",
+  "UGA": "STOP",
+}
+
+export const translate = (rna) => {
+  if (!rna) {
+    return [];
+  }
+
+  const codons = rna.match(/.{1,3}/g);
+  let proteins = [];
+
+  for (let codon of codons) {
+    let protein = PROTEINS[codon];
+
+    if (protein === "STOP") {
+      break;
+    } else {
+      proteins.push(protein);
+    }
+  }
+
+  return proteins;
+};
