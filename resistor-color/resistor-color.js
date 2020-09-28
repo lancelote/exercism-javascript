c654d0776c19fcdc4d1de95df29ad73503927791
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-09-28 14:14:43 +0300

    "Resistor Color" exercise

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,18 @@
+const COLOR_MAP = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+}
+
+export const colorCode = (color) => {
+  return COLOR_MAP[color];
+};
+
+export const COLORS = Object.getOwnPropertyNames(COLOR_MAP);
