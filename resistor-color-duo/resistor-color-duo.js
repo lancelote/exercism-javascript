659c6b9b83714828502d784bafb2cceb98150a11
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-10-09 15:51:02 +0300

    "Resistor Color Duo" changes after review
    - Number() instead of parseInt() as we are sure in input
    - Array instead of the object
    - Destructing assignment instead of an access via index

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,16 +1,19 @@
-const COLORS = {
-  "black": 0,
-  "brown": 1,
-  "red": 2,
-  "orange": 3,
-  "yellow": 4,
-  "green": 5,
-  "blue": 6,
-  "violet": 7,
-  "grey": 8,
-  "white": 9,
-}
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+]
 
 export const decodedValue = (colors) => {
-  return parseInt(`${COLORS[colors[0]]}${COLORS[colors[1]]}`);
+  let [firstColor, secondColor] = colors;
+  let firstColorIndex = COLORS.indexOf(firstColor);
+  let secondColorIndex = COLORS.indexOf(secondColor);
+  return Number(`${firstColorIndex}${secondColorIndex}`);
 };
