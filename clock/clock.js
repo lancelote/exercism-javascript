c16460e6b181810b26225de23dd7e16f6644bc5e
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-10-16 22:20:26 +0300

    "Clock" exercise

---
+++ clock/clock.js
@@ -0,0 +1,33 @@
+const formatNumber = number => number.toString().padStart(2, "0")
+
+
+export class Clock {
+  constructor(hour, minutes = 0) {
+    this.minutes = minutes % 60;
+    this.hour = (hour + Math.floor(minutes / 60)) % 24;
+
+    if (this.hour < 0) {
+      this.hour += 24;
+    }
+
+    if (this.minutes < 0) {
+      this.minutes += 60;
+    }
+  }
+
+  toString() {
+    return `${formatNumber(this.hour)}:${formatNumber(this.minutes)}`;
+  }
+
+  plus(minutes) {
+    return new Clock(this.hour, this.minutes + minutes);
+  }
+
+  minus(minutes) {
+    return new Clock(this.hour, this.minutes - minutes);
+  }
+
+  equals(other) {
+    return this.hour === other.hour && this.minutes === other.minutes;
+  }
+}
