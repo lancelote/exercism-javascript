cc92c96e85fbe11e7e847e5592776f7607e89566
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-10-23 22:49:42 +0300

    "ETL" exercise solution

---
+++ etl/etl.js
@@ -0,0 +1,9 @@
+export const transform = scoreToLetters => {
+  let letterToScore = {};
+  for (const [score, letters] of Object.entries(scoreToLetters)) {
+    for (const letter of letters) {
+      letterToScore[letter.toLowerCase()] = Number(score);
+    }
+  }
+  return letterToScore;
+};
