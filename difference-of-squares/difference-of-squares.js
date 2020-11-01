802124c0fba4b75f714047034a6c66314d6582af
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-11-01 20:33:09 +0300

    "Difference Of Squares" exercise

---
+++ difference-of-squares/difference-of-squares.js
@@ -0,0 +1,17 @@
+export class Squares {
+  constructor(n) {
+    this.n = n;
+  }
+
+  get sumOfSquares() {
+    return this.n * (this.n + 1) * (2 * this.n + 1) / 6;
+  }
+
+  get squareOfSum() {
+    return Math.pow(this.n * (this.n + 1) / 2, 2);
+  }
+
+  get difference() {
+    return this.squareOfSum - this.sumOfSquares;
+  }
+}
