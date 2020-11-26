ec7434ed26e1209c018ad2b0e0ab72578afa4fe9
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-11-26 12:47:34 +0300

    Matrix: changes after review
    - Use map where possible instead of for loop
    - Leave constructor minimalistic
    - Extract rows calculation into a separate method

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,25 +1,22 @@
 export class Matrix {
   constructor(string) {
-    this.rows = [];
-
-    for (const stringRow of string.split("\n")) {
-      const intRow = stringRow.split(" ").map(item => Number(item));
-      this.rows.push(intRow);
-    }
+    this.stringRows = string.split("\n");
   }
 
   get rowLength() {
     return this.rows[0].length;
   }
 
+  get rows() {
+    return this.stringRows.map(stringRow => {
+      return stringRow.split(" ").map(item => Number(item));
+    });
+  }
+
   get columns() {
     let cols = [];
     for (let i = 0; i < this.rowLength; i++) {
-      let col = [];
-      for (const row of this.rows) {
-        col.push(row[i]);
-      }
-      cols.push(col);
+      cols.push(this.rows.map(row => row[i]));
     }
     return cols;
   }
